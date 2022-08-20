a81f7bd41c2b1b4453224b94907b690afcacf5e1
andeya <andeyalee@outlook.com>
2022-08-21 00:56:26 +0800

    feat: add ord

---
+++ ord/ordering.go
@@ -0,0 +1,49 @@
+package ord
+
+import "github.com/andeya/gust/digit"
+
+type Ord interface {
+	digit.Digit | ~string | ~uintptr
+}
+
+type Ordering struct {
+	cmp int8
+}
+
+func Less() Ordering {
+	return Ordering{cmp: -1}
+}
+
+func Equal() Ordering {
+	return Ordering{cmp: 0}
+}
+
+func Greater() Ordering {
+	return Ordering{cmp: 1}
+}
+
+func Compare[T Ord](a, b T) Ordering {
+	if a < b {
+		return Less()
+	}
+	if a == b {
+		return Equal()
+	}
+	return Greater()
+}
+
+func (o Ordering) Is(ord Ordering) bool {
+	return o.cmp == ord.cmp
+}
+
+func (o Ordering) IsLess() bool {
+	return o.cmp == -1
+}
+
+func (o Ordering) IsEqual() bool {
+	return o.cmp == 0
+}
+
+func (o Ordering) IsGreater() bool {
+	return o.cmp == 1
+}
