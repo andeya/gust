da4cd4bc8490fa68bc89562589adbda6ac71be14
andeya <andeyalee@outlook.com>
2022-09-02 01:08:41 +0800

    docs: update Option tests

--- option_test.go
+++ option_test.go
@@ -2,51 +2,11 @@ package gust_test
 
 import (
 	"encoding/json"
-	"fmt"
-	"strconv"
 	"testing"
 
 	"github.com/andeya/gust"
-	"github.com/andeya/gust/opt"
 	"github.com/stretchr/testify/assert"
 )
-
-func ExampleOption() {
-	type A struct {
-		X int
-	}
-	var a = gust.Some(A{X: 1})
-	fmt.Println(a.IsSome(), a.IsNone())
-
-	var b = gust.None[A]()
-	fmt.Println(b.IsSome(), b.IsNone())
-
-	var x = b.UnwrapOr(A{X: 2})
-	fmt.Println(x)
-
-	var c *A
-	fmt.Println(gust.Ptr(c).IsNone())
-	c = new(A)
-	fmt.Println(gust.Ptr(c).IsNone())
-
-	type B struct {
-		Y string
-	}
-	var d = opt.Map(a, func(t A) B {
-		return B{
-			Y: strconv.Itoa(t.X),
-		}
-	})
-	fmt.Println(d)
-
-	// Output:
-	// true false
-	// false true
-	// {2}
-	// true
-	// false
-	// Some({1})
-}
 
 func TestOptionJSON(t *testing.T) {
 	var r = gust.None[any]()
