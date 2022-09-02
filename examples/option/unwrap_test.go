99e8c4355689786c1339480018def8bc76c1ba2d
andeya <andeyalee@outlook.com>
2022-09-02 12:13:48 +0800

    Create unwrap_test.go

---
+++ examples/option/unwrap_test.go
@@ -0,0 +1,20 @@
+package option_test
+
+import (
+	"testing"
+
+	"github.com/andeya/gust"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOption_Unwrap(t *testing.T) {
+	{
+		var x = gust.Some("air")
+		assert.Equal(t, "air", x.Unwrap())
+	}
+	defer func() {
+		assert.Equal(t, "call Option[string].Unwrap() on none", recover())
+	}()
+	var x = gust.None[string]()
+	x.Unwrap()
+}
