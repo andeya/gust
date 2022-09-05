a94344917d910e81592a93791780f2ac37a236e6
andeya <andeyalee@outlook.com>
2022-09-05 18:31:22 +0800

    chore: add iterator test

---
+++ examples/iterator/iter_overview_test.go
@@ -0,0 +1,20 @@
+package iterator_test
+
+import (
+	"fmt"
+
+	"github.com/andeya/gust/iter"
+)
+
+func ExampleIterator() {
+	var v = []int{1, 2, 3, 4, 5}
+	iter.FromVec(v).ForEach(func(x int) {
+		fmt.Printf("%d\n", x)
+	})
+	// Output:
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
+}
