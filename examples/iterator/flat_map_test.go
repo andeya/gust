0916769be4c99f23bb90900e2dd3f332cff21d2a
andeya <andeyalee@outlook.com>
2022-09-07 23:19:00 +0800

    Create flat_map_test.go

---
+++ examples/iterator/flat_map_test.go
@@ -0,0 +1,23 @@
+package iterator_test
+
+import (
+	"testing"
+
+	"github.com/andeya/gust/iter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlatMap(t *testing.T) {
+	var c = make(chan string, 10)
+	for _, i := range []iter.Iterator[string]{
+		iter.FromElements("alpha", "beta", "gamma").Inspect(func(v string) {
+			c <- v
+		}),
+		iter.FromChan(c),
+	} {
+		var merged = iter.FlatMap(i, func(t string) iter.Iterator[rune] {
+			return iter.FromString[rune](t)
+		}).Collect()
+		assert.Equal(t, "alphabetagamma", string(merged))
+	}
+}
