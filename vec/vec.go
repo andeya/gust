1300a937ba2a1ee205d55d7d110c97c8f80a69f9
andeya <andeyalee@outlook.com>
2022-09-20 17:55:23 +0800

    feat: add vec

---
+++ vec/vec.go
@@ -0,0 +1,544 @@
+package vec
+
+// One try to return the first element, otherwise return zero value.
+func One[T any](s []T) T {
+	if len(s) > 0 {
+		return s[0]
+	}
+	return zero[T]()
+}
+
+// Copy creates a copy of the slice.
+func Copy[T any](s []T) []T {
+	r := make([]T, len(s))
+	copy(r, s)
+	return r
+}
+
+// CopyWithin copies part of a slice to another location in the current slice.
+// @target
+//
+//	Zero-based index at which to copy the sequence to. If negative, target will be counted from the end.
+//
+// @start
+//
+//	Zero-based index at which to start copying elements from. If negative, start will be counted from the end.
+//
+// @end
+//
+//	Zero-based index at which to end copying elements from. CopyWithin copies up to but not including end.
+//	If negative, end will be counted from the end.
+//	If end is omitted, CopyWithin will copy until the last index (default to len(s)).
+func CopyWithin[T any](s []T, target, start int, end ...int) {
+	target = fixIndex(len(s), target, true)
+	if target == len(s) {
+		return
+	}
+	sub := Slice(s, start, end...)
+	for i, v := range sub {
+		s[target+i] = v
+	}
+}
+
+// Every tests whether all elements in the slice pass the test implemented by the provided function.
+// NOTE:
+//
+//	Calling this method on an empty slice will return true for any condition!
+func Every[T any](s []T, fn func(s []T, k int, v T) bool) bool {
+	for k, v := range s {
+		if !fn(s, k, v) {
+			return false
+		}
+	}
+	return true
+}
+
+// Fill changes all elements in the current slice to a value, from a start index to an end index.
+// @value
+//
+//	Zero-based index at which to copy the sequence to. If negative, target will be counted from the end.
+//
+// @start
+//
+//	Zero-based index at which to start copying elements from. If negative, start will be counted from the end.
+//
+// @end
+//
+//	Zero-based index at which to end copying elements from. CopyWithin copies up to but not including end.
+//	If negative, end will be counted from the end.
+//	If end is omitted, CopyWithin will copy until the last index (default to len(s)).
+func Fill[T any](s []T, value T, start int, end ...int) {
+	fixedStart, fixedEnd, ok := fixRange(len(s), start, end...)
+	if !ok {
+		return
+	}
+	for i := fixedStart; i < fixedEnd; i++ {
+		s[i] = value
+	}
+}
+
+// Filter creates a new slice with all elements that pass the test implemented by the provided function.
+func Filter[T any](s []T, fn func(s []T, k int, v T) bool) []T {
+	ret := make([]T, 0)
+	for k, v := range s {
+		if fn(s, k, v) {
+			ret = append(ret, v)
+		}
+	}
+	return ret
+}
+
+// Find returns the key-value of the first element in the provided slice that satisfies the provided testing function.
+// NOTE:
+//
+//	If not found, k = -1
+func Find[T any](s []T, fn func(s []T, k int, v T) bool) (k int, v T) {
+	for k, v := range s {
+		if fn(s, k, v) {
+			return k, v
+		}
+	}
+	return -1, zero[T]()
+}
+
+// Includes determines whether a slice includes a certain value among its entries.
+// @fromIndex
+//
+//	The index to start the search at. Defaults to 0.
+func Includes[T comparable](s []T, valueToFind T, fromIndex ...int) bool {
+	return IndexOf(s, valueToFind, fromIndex...) > -1
+}
+
+// IndexOf returns the first index at which a given element can be found in the slice, or -1 if it is not present.
+// @fromIndex
+//
+//	The index to start the search at. Defaults to 0.
+func IndexOf[T comparable](s []T, searchElement T, fromIndex ...int) int {
+	idx := getFromIndex(len(s), fromIndex...)
+	for k, v := range s[idx:] {
+		if searchElement == v {
+			return k + idx
+		}
+	}
+	return -1
+}
+
+// LastIndexOf returns the last index at which a given element can be found in the slice, or -1 if it is not present.
+// @fromIndex
+//
+//	The index to start the search at. Defaults to 0.
+func LastIndexOf[T comparable](s []T, searchElement T, fromIndex ...int) int {
+	idx := getFromIndex(len(s), fromIndex...)
+	for i := len(s) - 1; i >= idx; i-- {
+		if searchElement == s[i] {
+			return i
+		}
+	}
+	return -1
+}
+
+// Map creates a new slice populated with the results of calling a provided function
+// on every element in the calling slice.
+func Map[T any](s []T, fn func(s []T, k int, v T) T) []T {
+	ret := make([]T, len(s))
+	for k, v := range s {
+		ret[k] = fn(s, k, v)
+	}
+	return ret
+}
+
+// Pop removes the last element from a slice and returns that element.
+// This method changes the length of the slice.
+func Pop[T any](s *[]T) (T, bool) {
+	a := *s
+	if len(a) == 0 {
+		return zero[T](), false
+	}
+	lastIndex := len(a) - 1
+	last := a[lastIndex]
+	a = a[:lastIndex]
+	*s = a[:len(a):len(a)]
+	return last, true
+}
+
+// Push adds one or more elements to the end of a slice and returns the new length of the slice.
+func Push[T any](s *[]T, element ...T) int {
+	*s = append(*s, element...)
+	return len(*s)
+}
+
+// PushDistinct adds one or more new elements that do not exist in the current slice at the end.
+func PushDistinct[T comparable](s []T, element ...T) []T {
+L:
+	for _, v := range element {
+		for _, vv := range s {
+			if vv == v {
+				continue L
+			}
+		}
+		s = append(s, v)
+	}
+	return s
+}
+
+// Reduce executes a reducer function (that you provide) on each element of the slice,
+// resulting in a single output value.
+// @accumulator
+//
+//	The accumulator accumulates callback's return values.
+//	It is the accumulated value previously returned in the last invocation of the callback—or initialValue,
+//	if it was supplied (see below).
+//
+// @initialValue
+//
+//	A value to use as the first argument to the first call of the callback.
+//	If no initialValue is supplied, the first element in the slice will be used and skipped.
+func Reduce[T any](s []T, fn func(s []T, k int, v, accumulator T) T, initialValue ...T) T {
+	if len(s) == 0 {
+		return zero[T]()
+	}
+	start := 0
+	acc := s[start]
+	if len(initialValue) > 0 {
+		acc = initialValue[0]
+	} else {
+		start += 1
+	}
+	for i := start; i < len(s); i++ {
+		acc = fn(s, i, s[i], acc)
+	}
+	return acc
+}
+
+// ReduceRight applies a function against an accumulator and each value of the slice (from right-to-left)
+// to reduce it to a single value.
+// @accumulator
+//
+//	The accumulator accumulates callback's return values.
+//	It is the accumulated value previously returned in the last invocation of the callback—or initialValue,
+//	if it was supplied (see below).
+//
+// @initialValue
+//
+//	A value to use as the first argument to the first call of the callback.
+//	If no initialValue is supplied, the first element in the slice will be used and skipped.
+func ReduceRight[T any](s []T, fn func(s []T, k int, v, accumulator T) T, initialValue ...T) T {
+	if len(s) == 0 {
+		return zero[T]()
+	}
+	end := len(s) - 1
+	acc := s[end]
+	if len(initialValue) > 0 {
+		acc = initialValue[0]
+	} else {
+		end -= 1
+	}
+	for i := end; i >= 0; i-- {
+		acc = fn(s, i, s[i], acc)
+	}
+	return acc
+}
+
+// Reverse reverses a slice in place.
+func Reverse[T any](s []T) {
+	first := 0
+	last := len(s) - 1
+	for first < last {
+		s[first], s[last] = s[last], s[first]
+		first++
+		last--
+	}
+}
+
+// Shift removes the first element from a slice and returns that removed element.
+// This method changes the length of the slice.
+func Shift[T any](s *[]T) (T, bool) {
+	a := *s
+	if len(a) == 0 {
+		return zero[T](), false
+	}
+	first := a[0]
+	a = a[1:]
+	*s = a[:len(a):len(a)]
+	return first, true
+}
+
+// Slice returns a copy of a portion of a slice into a new slice object selected
+// from begin to end (end not included) where begin and end represent the index of items in that slice.
+// The original slice will not be modified.
+func Slice[T any](s []T, begin int, end ...int) []T {
+	fixedStart, fixedEnd, ok := fixRange(len(s), begin, end...)
+	if !ok {
+		return []T{}
+	}
+	return Copy[T](s[fixedStart:fixedEnd])
+}
+
+// Some tests whether at least one element in the slice passes the test implemented by the provided function.
+// NOTE:
+//
+//	Calling this method on an empty slice returns false for any condition!
+func Some[T any](s []T, fn func(s []T, k int, v T) bool) bool {
+	for k, v := range s {
+		if fn(s, k, v) {
+			return true
+		}
+	}
+	return false
+}
+
+// Splice changes the contents of a slice by removing or replacing
+// existing elements and/or adding new elements in place.
+func Splice[T any](s *[]T, start, deleteCount int, items ...T) {
+	a := *s
+	if deleteCount < 0 {
+		deleteCount = 0
+	}
+	start, end, _ := fixRange(len(a), start, start+1+deleteCount)
+	deleteCount = end - start - 1
+	for i := 0; i < len(items); i++ {
+		if deleteCount > 0 {
+			// replace
+			a[start] = items[i]
+			deleteCount--
+			start++
+		} else {
+			// insert
+			lastSlice := Copy[T](a[start:])
+			items = items[i:]
+			a = append(a[:start], items...)
+			a = append(a[:start+len(items)], lastSlice...)
+			*s = a[:len(a):len(a)]
+			return
+		}
+	}
+	if deleteCount > 0 {
+		a = append(a[:start], a[start+1+deleteCount:]...)
+	}
+	*s = a[:len(a):len(a)]
+}
+
+// Unshift adds one or more elements to the beginning of a slice and returns the new length of the slice.
+func Unshift[T any](s *[]T, element ...T) int {
+	*s = append(element, *s...)
+	return len(*s)
+}
+
+// UnshiftDistinct adds one or more new elements that do not exist in the current slice to the beginning
+// and returns the new length of the slice.
+func UnshiftDistinct[T comparable](s *[]T, element ...T) int {
+	a := *s
+	if len(element) == 0 {
+		return len(a)
+	}
+	m := make(map[T]bool, len(element))
+	r := make([]T, 0, len(a)+len(element))
+L:
+	for _, v := range element {
+		if m[v] {
+			continue
+		}
+		m[v] = true
+		for _, vv := range a {
+			if vv == v {
+				continue L
+			}
+		}
+		r = append(r, v)
+	}
+	r = append(r, a...)
+	*s = r[:len(r):len(r)]
+	return len(r)
+}
+
+// RemoveFirst removes the first matched elements from the slice,
+// and returns the new length of the slice.
+func RemoveFirst[T comparable](p *[]T, elements ...T) int {
+	a := *p
+	m := make(map[interface{}]struct{}, len(elements))
+	for _, element := range elements {
+		if _, ok := m[element]; ok {
+			continue
+		}
+		m[element] = struct{}{}
+		for k, v := range a {
+			if v == element {
+				a = append(a[:k], a[k+1:]...)
+				break
+			}
+		}
+	}
+	n := len(a)
+	*p = a[:n:n]
+	return n
+}
+
+// RemoveEvery removes all the elements from the slice,
+// and returns the new length of the slice.
+func RemoveEvery[T comparable](p *[]T, elements ...T) int {
+	a := *p
+	m := make(map[interface{}]struct{}, len(elements))
+	for _, element := range elements {
+		if _, ok := m[element]; ok {
+			continue
+		}
+		m[element] = struct{}{}
+		for i := 0; i < len(a); i++ {
+			if a[i] == element {
+				a = append(a[:i], a[i+1:]...)
+				i--
+			}
+		}
+	}
+	n := len(a)
+	*p = a[:n:n]
+	return n
+}
+
+// Concat is used to merge two or more slices.
+// This method does not change the existing slices, but instead returns a new slice.
+func Concat[T any](s ...[]T) []T {
+	var totalLen int
+	for _, v := range s {
+		totalLen += len(v)
+	}
+	ret := make([]T, totalLen)
+	dst := ret
+	for _, v := range s {
+		n := copy(dst, v)
+		dst = dst[n:]
+	}
+	return ret
+}
+
+// Intersect calculates intersection of two or more slices,
+// and returns the count of each element.
+func Intersect[T comparable](s ...[]T) (intersectCount map[T]int) {
+	if len(s) == 0 {
+		return nil
+	}
+	for _, v := range s {
+		if len(v) == 0 {
+			return nil
+		}
+	}
+	counts := make([]map[T]int, len(s))
+	for k, v := range s {
+		counts[k] = vecDistinct(v, nil)
+	}
+	intersectCount = counts[0]
+L:
+	for k, v := range intersectCount {
+		for _, c := range counts[1:] {
+			v2 := c[k]
+			if v2 == 0 {
+				delete(intersectCount, k)
+				continue L
+			}
+			if v > v2 {
+				v = v2
+			}
+		}
+		intersectCount[k] = v
+	}
+	return intersectCount
+}
+
+// Distinct calculates the count of each different element,
+// and only saves these different elements in place if changeSlice is true.
+func Distinct[T comparable](s *[]T, changeSlice bool) (distinctCount map[T]int) {
+	if !changeSlice {
+		return vecDistinct(*s, nil)
+	}
+	a := (*s)[:0]
+	distinctCount = vecDistinct(*s, &a)
+	n := len(distinctCount)
+	*s = a[:n:n]
+	return distinctCount
+}
+
+func vecDistinct[T comparable](src []T, dst *[]T) map[T]int {
+	m := make(map[T]int, len(src))
+	if dst == nil {
+		for _, v := range src {
+			n := m[v]
+			m[v] = n + 1
+		}
+	} else {
+		a := *dst
+		for _, v := range src {
+			n := m[v]
+			m[v] = n + 1
+			if n == 0 {
+				a = append(a, v)
+			}
+		}
+		*dst = a
+	}
+	return m
+}
+
+// SetsUnion calculates between multiple collections: set1 ∪ set2 ∪ others...
+// This method does not change the existing slices, but instead returns a new slice.
+func SetsUnion[T comparable](set1, set2 []T, others ...[]T) []T {
+	m := make(map[T]struct{}, len(set1)+len(set2))
+	r := make([]T, 0, len(m))
+	for _, set := range append([][]T{set1, set2}, others...) {
+		for _, v := range set {
+			_, ok := m[v]
+			if ok {
+				continue
+			}
+			r = append(r, v)
+			m[v] = struct{}{}
+		}
+	}
+	return r
+}
+
+// SetsIntersect calculates between multiple collections: set1 ∩ set2 ∩ others...
+// This method does not change the existing slices, but instead returns a new slice.
+func SetsIntersect[T comparable](set1, set2 []T, others ...[]T) []T {
+	sets := append([][]T{set2}, others...)
+	setsCount := make([]map[T]int, len(sets))
+	for k, v := range sets {
+		setsCount[k] = vecDistinct(v, nil)
+	}
+	m := make(map[T]struct{}, len(set1))
+	r := make([]T, 0, len(m))
+L:
+	for _, v := range set1 {
+		if _, ok := m[v]; ok {
+			continue
+		}
+		m[v] = struct{}{}
+		for _, m2 := range setsCount {
+			if m2[v] == 0 {
+				continue L
+			}
+		}
+		r = append(r, v)
+	}
+	return r
+}
+
+// SetsDifference calculates between multiple collections: set1 - set2 - others...
+// This method does not change the existing slices, but instead returns a new slice.
+func SetsDifference[T comparable](set1, set2 []T, others ...[]T) []T {
+	m := make(map[T]struct{}, len(set1))
+	r := make([]T, 0, len(set1))
+	sets := append([][]T{set2}, others...)
+	for _, v := range sets {
+		inter := SetsIntersect(set1, v)
+		for _, v := range inter {
+			m[v] = struct{}{}
+		}
+	}
+	for _, v := range set1 {
+		if _, ok := m[v]; !ok {
+			r = append(r, v)
+			m[v] = struct{}{}
+		}
+	}
+	return r
+}
---
+++ vec/vec_test.go
@@ -0,0 +1,260 @@
+package vec
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConcat(t *testing.T) {
+	a := []string{"a", "0"}
+	b := []string{"b", "1"}
+	c := []string{"c", "2"}
+	r := Concat(a, b, c)
+	assert.Equal(t, []string{"a", "0", "b", "1", "c", "2"}, r)
+}
+
+func TestIntersect(t *testing.T) {
+	x := []string{"a", "b", "a", "b", "b", "a", "a"}
+	y := []string{"a", "b", "c", "a", "b", "c", "b", "c", "c"}
+	z := []string{"a", "b", "c", "d", "a", "b", "c", "d", "b", "c", "d", "c", "d", "d"}
+	r := Intersect(x, y, z)
+	assert.Equal(t, map[string]int{"a": 2, "b": 3}, r)
+}
+
+func TestCopyWithin(t *testing.T) {
+	slice := []string{"a", "b", "c", "d", "e"}
+	CopyWithin(slice, 0, 3, 4)
+	assert.Equal(t, []string{"d", "b", "c", "d", "e"}, slice)
+	CopyWithin(slice, 1, -2)
+	assert.Equal(t, []string{"d", "d", "e", "d", "e"}, slice)
+}
+
+func TestEvery(t *testing.T) {
+	slice := []string{"1", "30", "39", "29", "10", "13"}
+	isBelowThreshold := Every(slice, func(s []string, k int, v string) bool {
+		i, _ := strconv.Atoi(v)
+		return i < 40
+	})
+	assert.Equal(t, true, isBelowThreshold)
+}
+
+func TestFill(t *testing.T) {
+	slice := []string{"a", "b", "c", "d"}
+	Fill(slice, "?", 2, 4)
+	assert.Equal(t, []string{"a", "b", "?", "?"}, slice)
+	Fill(slice, "e", -1)
+	assert.Equal(t, []string{"a", "b", "?", "e"}, slice)
+}
+
+func TestFilter(t *testing.T) {
+	slice := []string{"spray", "limit", "elite", "exuberant", "destruction", "present"}
+	result := Filter(slice, func(s []string, k int, v string) bool {
+		return len(v) > 6
+	})
+	assert.Equal(t, []string{"exuberant", "destruction", "present"}, result)
+}
+
+func TestFind(t *testing.T) {
+	slice := []string{"spray", "limit", "elite", "exuberant", "destruction", "present"}
+	k, v := Find(slice, func(s []string, k int, v string) bool {
+		return len(v) > 6
+	})
+	assert.Equal(t, 3, k)
+	assert.Equal(t, "exuberant", v)
+}
+
+func TestIncludes(t *testing.T) {
+	slice := []string{"spray", "limit", "elite", "exuberant", "destruction", "present"}
+	had := Includes(slice, "limit")
+	assert.True(t, had)
+	had = Includes(slice, "limit", 1)
+	assert.True(t, had)
+	had = Includes(slice, "limit", 2)
+	assert.False(t, had)
+}
+
+func TestIndexOf(t *testing.T) {
+	slice := []string{"spray", "limit", "elite", "exuberant", "destruction", "present"}
+	idx := IndexOf(slice, "limit")
+	assert.Equal(t, 1, idx)
+	idx = IndexOf(slice, "limit", 1)
+	assert.Equal(t, 1, idx)
+	idx = IndexOf(slice, "limit", 10)
+	assert.Equal(t, -1, idx)
+}
+
+func TestLastIndexOf(t *testing.T) {
+	slice := []string{"Dodo", "Tiger", "Penguin", "Dodo"}
+	idx := LastIndexOf(slice, "Dodo")
+	assert.Equal(t, 3, idx)
+	idx = LastIndexOf(slice, "Dodo", 1)
+	assert.Equal(t, 3, idx)
+	idx = LastIndexOf(slice, "Dodo", 10)
+	assert.Equal(t, -1, idx)
+	idx = LastIndexOf(slice, "?")
+	assert.Equal(t, -1, idx)
+}
+
+func TestMap(t *testing.T) {
+	slice := []string{"Dodo", "Tiger", "Penguin", "Dodo"}
+	ret := Map(slice, func(s []string, k int, v string) string {
+		return strconv.Itoa(k+1) + ":" + v
+	})
+	assert.Equal(t, []string{"1:Dodo", "2:Tiger", "3:Penguin", "4:Dodo"}, ret)
+}
+
+func TestPop(t *testing.T) {
+	slice := []string{"kale", "tomato"}
+	last, ok := Pop(&slice)
+	assert.True(t, ok)
+	assert.Equal(t, "tomato", last)
+	last, ok = Pop(&slice)
+	assert.True(t, ok)
+	assert.Equal(t, "kale", last)
+	last, ok = Pop(&slice)
+	assert.False(t, ok)
+	assert.Equal(t, "", last)
+}
+
+func TestPushDistinct(t *testing.T) {
+	slice := []string{"1", "2", "3", "4"}
+	slice = PushDistinct(slice, "1", "5", "6", "1", "5", "6")
+	assert.Equal(t, []string{"1", "2", "3", "4", "5", "6"}, slice)
+}
+
+func TestReduce(t *testing.T) {
+	slice := []string{"1", "2", "3", "4"}
+	reducer := Reduce(slice, func(s []string, k int, v string, accumulator string) string {
+		return accumulator + "+" + v
+	})
+	assert.Equal(t, "1+2+3+4", reducer)
+	reducer = Reduce(slice, func(s []string, k int, v string, accumulator string) string {
+		return accumulator + "+" + v
+	}, "100")
+	assert.Equal(t, "100+1+2+3+4", reducer)
+}
+
+func TestReduceRight(t *testing.T) {
+	slice := []string{"1", "2", "3", "4"}
+	reducer := ReduceRight(slice, func(s []string, k int, v string, accumulator string) string {
+		return accumulator + "+" + v
+	})
+	assert.Equal(t, "4+3+2+1", reducer)
+	reducer = ReduceRight(slice, func(s []string, k int, v string, accumulator string) string {
+		return accumulator + "+" + v
+	}, "100")
+	assert.Equal(t, "100+4+3+2+1", reducer)
+}
+
+func TestReverse(t *testing.T) {
+	slice := []string{"1", "2", "3", "4"}
+	Reverse(slice)
+	assert.Equal(t, []string{"4", "3", "2", "1"}, slice)
+}
+
+func TestShift(t *testing.T) {
+	slice := []string{"kale", "tomato"}
+	first, ok := Shift(&slice)
+	assert.True(t, ok)
+	assert.Equal(t, "kale", first)
+	first, ok = Pop(&slice)
+	assert.True(t, ok)
+	assert.Equal(t, "tomato", first)
+	first, ok = Pop(&slice)
+	assert.False(t, ok)
+	assert.Equal(t, "", first)
+}
+
+func TestSlice(t *testing.T) {
+	slice := []string{"a", "b", "c", "d", "e"}
+	sub := Slice(slice, 3)
+	assert.Equal(t, []string{"d", "e"}, sub)
+	sub = Slice(slice, 3, 4)
+	assert.Equal(t, []string{"d"}, sub)
+	sub = Slice(slice, 1, -2)
+	assert.Equal(t, []string{"b", "c"}, sub)
+	sub[0] = "x"
+	assert.Equal(t, []string{"x", "c"}, sub)
+	assert.Equal(t, []string{"a", "b", "c", "d", "e"}, slice)
+}
+
+func TestSome(t *testing.T) {
+	slice := []string{"1", "30", "39", "29", "10", "13"}
+	even := Some(slice, func(s []string, k int, v string) bool {
+		i, _ := strconv.Atoi(v)
+		return i%2 == 0
+	})
+	assert.Equal(t, true, even)
+}
+
+func TestSplice(t *testing.T) {
+	slice := []string{"0", "1", "2", "3", "4"}
+	Splice(&slice, 0, 0, "a", "b")
+	assert.Equal(t, []string{"a", "b", "0", "1", "2", "3", "4"}, slice)
+
+	slice = []string{"0", "1", "2", "3", "4"}
+	Splice(&slice, 10, 0, "a", "b")
+	assert.Equal(t, []string{"0", "1", "2", "3", "4", "a", "b"}, slice)
+
+	slice = []string{"0", "1", "2", "3", "4"}
+	Splice(&slice, 1, 0, "a", "b")
+	assert.Equal(t, []string{"0", "a", "b", "1", "2", "3", "4"}, slice)
+
+	slice = []string{"0", "1", "2", "3", "4"}
+	Splice(&slice, 1, 2, "a", "b")
+	assert.Equal(t, []string{"0", "a", "b", "3", "4"}, slice)
+
+	slice = []string{"0", "1", "2", "3", "4"}
+	Splice(&slice, 1, 10, "a", "b")
+	assert.Equal(t, []string{"0", "a", "b"}, slice)
+}
+
+func TestUnshift(t *testing.T) {
+	slice := []string{"0", "1", "2", "3", "4"}
+	n := Unshift(&slice, "a", "b")
+	assert.Equal(t, len(slice), n)
+	assert.Equal(t, []string{"a", "b", "0", "1", "2", "3", "4"}, slice)
+}
+
+func TestUnshiftDistinct(t *testing.T) {
+	slice := []string{"1", "2", "3", "4"}
+	n := UnshiftDistinct(&slice, "-1", "0", "-1", "0", "1", "1")
+	assert.Equal(t, len(slice), n)
+	assert.Equal(t, []string{"-1", "0", "1", "2", "3", "4"}, slice)
+}
+
+func TestDistinct(t *testing.T) {
+	slice := []string{"-1", "0", "-1", "0", "1", "1"}
+	distinctCount := Distinct(&slice, true)
+	assert.Equal(t, len(slice), len(distinctCount))
+	assert.Equal(t, []string{"-1", "0", "1"}, slice)
+	assert.Equal(t, map[string]int{"-1": 2, "0": 2, "1": 2}, distinctCount)
+}
+
+func TestRemoveFirst(t *testing.T) {
+	slice := []string{"-1", "0", "-1", "0", "1", "1"}
+	n := RemoveFirst(&slice, "0")
+	assert.Equal(t, len(slice), n)
+	assert.Equal(t, []string{"-1", "-1", "0", "1", "1"}, slice)
+}
+
+func TestRemoveEvery(t *testing.T) {
+	slice := []string{"-1", "0", "-1", "0", "1", "1"}
+	n := RemoveEvery(&slice, "0")
+	assert.Equal(t, len(slice), n)
+	assert.Equal(t, []string{"-1", "-1", "1", "1"}, slice)
+}
+
+func TestStringSet(t *testing.T) {
+	set1 := []string{"1", "2", "3", "6", "8"}
+	set2 := []string{"2", "3", "5", "0"}
+	set3 := []string{"2", "6", "7"}
+	un := SetsUnion(set1, set2, set3)
+	assert.Equal(t, []string{"1", "2", "3", "6", "8", "5", "0", "7"}, un)
+	in := SetsIntersect(set1, set2, set3)
+	assert.Equal(t, []string{"2"}, in)
+	di := SetsDifference(set1, set2, set3)
+	assert.Equal(t, []string{"1", "8"}, di)
+}
