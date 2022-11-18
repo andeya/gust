633d3f615171d3a1c4782e9c819f6716db9d6166
andeya <andeyalee@outlook.com>
2022-11-18 21:31:19 +0800

    feat: Add sync Map
    Change-Id: I44c3bb025e0c1b8c227acfc6023731bcb032d7c1

--- sync.go
+++ sync.go
@@ -131,3 +131,82 @@ func (m *RWMutex[T]) TryRLock() Option[T] {
 func (m *RWMutex[T]) RUnlock() {
 	m.inner.RUnlock()
 }
+
+// Map is like a Go map[interface{}]interface{} but is safe for concurrent use
+// by multiple goroutines without additional locking or coordination.
+// Loads, stores, and deletes run in amortized constant time.
+//
+// The Map type is specialized. Most code should use a plain Go map instead,
+// with separate locking or coordination, for better type safety and to make it
+// easier to maintain other invariants along with the map content.
+//
+// The Map type is optimized for two common use cases: (1) when the entry for a given
+// key is only ever written once but read many times, as in caches that only grow,
+// or (2) when multiple goroutines read, write, and overwrite entries for disjoint
+// sets of keys. In these two cases, use of a Map may significantly reduce lock
+// contention compared to a Go map paired with a separate Mutex or RWMutex.
+//
+// The zero Map is empty and ready for use. A Map must not be copied after first use.
+//
+// In the terminology of the Go memory model, Map arranges that a write operation
+// “synchronizes before” any read operation that observes the effect of the write, where
+// read and write operations are defined as follows.
+// Load, LoadAndDelete, LoadOrStore are read operations;
+// Delete, LoadAndDelete, and Store are write operations;
+// and LoadOrStore is a write operation when it returns loaded set to false.
+type Map[K any, V any] struct {
+	inner sync.Map
+}
+
+// Load returns the value stored in the map for a key, or nil if no
+// value is present.
+// The ok result indicates whether value was found in the map.
+func (m *Map[K, V]) Load(key K) Option[V] {
+	return BoolAssertOpt[V](m.inner.Load(key))
+}
+
+// Store sets the value for a key.
+func (m *Map[K, V]) Store(key K, value V) {
+	m.inner.Store(key, value)
+}
+
+// LoadOrStore returns the existing value for the key if present.
+// Otherwise, it stores the given value, and returns None.
+func (m *Map[K, V]) LoadOrStore(key any, value any) (existingValue Option[V]) {
+	return BoolAssertOpt[V](m.inner.LoadOrStore(key, value))
+}
+
+// LoadAndDelete deletes the value for a key, returning the previous value if any.
+func (m *Map[K, V]) LoadAndDelete(key any) (deletedValue Option[V]) {
+	return BoolAssertOpt[V](m.inner.LoadAndDelete(key))
+}
+
+// Delete deletes the value for a key.
+func (m *Map[K, V]) Delete(key any) {
+	m.inner.Delete(key)
+}
+
+// Range calls f sequentially for each key and value present in the map.
+// If f returns false, range stops the iteration.
+//
+// Range does not necessarily correspond to any consistent snapshot of the Map's
+// contents: no key will be visited more than once, but if the value for any key
+// is stored or deleted concurrently (including by f), Range may reflect any
+// mapping for that key from any point during the Range call. Range does not
+// block other methods on the receiver; even f itself may call any method on m.
+//
+// Range may be O(N) with the number of elements in the map even if f returns
+// false after a constant number of calls.
+func (m *Map[K, V]) Range(f func(key K, value V) bool) {
+	m.inner.Range(func(key any, value any) bool {
+		k, ok := key.(K)
+		if !ok {
+			return false
+		}
+		v, ok := value.(V)
+		if !ok {
+			return false
+		}
+		return f(k, v)
+	})
+}
